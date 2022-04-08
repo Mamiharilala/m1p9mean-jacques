eba7053924079be603a2634a791184e71b652d33
Jacques <mamiharilalajacques@gmail.com>
2022-04-08 20:33:22 +0300

    202204080833

--- angular/src/app/components/commande-livreur/commande-livreur.component.ts
+++ angular/src/app/components/commande-livreur/commande-livreur.component.ts
@@ -12,38 +12,57 @@ export class CommandeLivreurComponent implements OnInit {
 	results: any[] = [];
 	searchResults: any[] = [];
 	platNotAssigned: any[] = []; 
+	livreurs: any[] = [];
+	index:any;
 	constructor(private utilisateurService: UtilisateurService,private platService: PlatService) { }
 
 	ngOnInit(): void {
 		this.getSearchResults();
 		this.onFindAllNotAssigned();
+		this.onFindAllLivreurs();
 	}
+
 	getSearchResults(): void {
 		this.utilisateurService.getResultList().subscribe(sr => { Object.assign(this.searchResults, sr); });
 	}
 
 	searchOnKeyUp(event: any) {
 		let input = event.target.value;
-		//console.log('event.target.value: ' + input);
+ 		//console.log('event.target.value: ' + input);
 		//console.log('this.searchResults: ' + this.searchResults);
 		if (input.length > 1) {
-			this.results = this.searchFromArray(this.searchResults, input);
+			this.results = this.searchFromArray(this.livreurs, input);
 		}
 	}
 
 	searchFromArray(arr: any, regex: any) {
-		let matches = [], i;
+ 		let matches = [], i;
 		for (i = 0; i < arr.length; i++) {
-			if (arr[i].match(regex)) {
+			if (arr[i]['prenom'].match(regex)) {
 				matches.push(arr[i]);
+				this.index = i;
 			}
 		}
 		//console.log('matches: ' + matches);
 		return matches;
 	};
+	onFindAllLivreurs(){
+		const headers = { 'Authorization': '' + localStorage.getItem("token") };
+	   this.utilisateurService.getLivreurs(headers).subscribe(res => {
+		   this.livreurs = res['data'];
+		   console.log(this.livreurs.length);
+		   });
+		  
+   	}
 	onFindAllNotAssigned(){
-		this.platService.getAllNotAssigned().subscribe(res => {
+     	const headers = { 'Authorization': '' + localStorage.getItem("token") };
+		this.platService.getAllNotAssigned(headers).subscribe(res => {
 			this.platNotAssigned = res['data'];
   		  });
+	}
+	onAssign(){
+		console.log(this.results);
+		console.log(this.index);
+		
 	}
 }
